21fdc11950c3ed34e8cacd584476fb02fbcb137d
Kendall Roth <kroth@unaviamedia.ca>
2016-06-01 10:21:56 -0400

    Added removal section to configure script

--- configure.sh
+++ configure.sh
@@ -7,10 +7,18 @@ cd ~/
 mv ~/linux_configs/ ~/.configs/
 cd .configs
 
+# Remove any old files
+rm -i ~/.vim
+rm -i ~/.vimrc
+rm -i ~/.bashrc
+rm -i ~/.bash_profile
+rm -i ~/.gitconfig
+rm -i ~/.screenrc
+
 # Create the necessary symlinks
-ln -si ~/.vim
-ln -si ~/.vimrc
-ln -si ~/.bashrc
-ln -si ~/.bash_profile
-ln -si ~/.gitconfig
-ln -si ~/.screenrc
+ln -si ~/.configs/.vim ~/.vim
+ln -si ~/.configs/.vimrc ~/.vimrc
+ln -si ~/.configs/.bashrc ~/.bashrc
+ln -si ~/.configs/.bash_profile ~/.bash_profile
+ln -si ~/.configs/.gitconfig ~/.gitconfig
+ln -si ~/.configs/.screenrc ~/.screenrc
